ad8ad58ef9f4812d80ea57472f406967f7bafbe4
Ronit raj <ronitraj42041@gmail.com>
2025-03-07 19:54:03 +0530

    modified backend to support chosing no of letters

--- backend/index.js
+++ backend/index.js
@@ -4,20 +4,26 @@ const fs=require('fs');
 const { type } = require('os');
 const port=8000;
 const frontend='http://localhost:5500'
-const dictFile='dict2.json'
+let dictFile='dict5.json' //default values 
+let noOfLetters=5 //default values 
 
 var words=null
 
 function readDict(){
     dict=fs.readFileSync(dictFile,'utf-8');
     words=JSON.parse(dict)
-    
 }
 app.listen(port,()=>{
     console.log(`listning on port${port}`);
-    readDict()
 })
+
+//getting the no of letters from the dict route because it 
+//is the first route to be called by the front-end 
 app.get('/dict',(req,res)=>{
+    noOfLetters=req.query.letters
+    dictFile=`dict${noOfLetters}.json`
+    readDict()
+
     res.statusCode=200
     res.set('Access-Control-Allow-Origin','*')
     res.send(dict);
